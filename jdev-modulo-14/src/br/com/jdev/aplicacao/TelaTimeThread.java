888a87653c55bbfce696c9165cd121550131333c
Igor Santos Oliveira <iigor315@hotmail.com>
2022-09-02 16:15:28 -0300

    Conhecendo o GridBagConstraints gerenciador de posicionamento e layout

--- jdev-modulo-14/src/br/com/jdev/aplicacao/TelaTimeThread.java
+++ jdev-modulo-14/src/br/com/jdev/aplicacao/TelaTimeThread.java
@@ -1,24 +1,57 @@
 package br.com.jdev.aplicacao;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 
 public class TelaTimeThread extends JDialog {
 	
+//	Painel de componentes
+	private JPanel jPanel = new JPanel(new GridBagLayout());
+	
+	private JLabel descricaoHora = new JLabel("Time da thread1");
+	private JTextField mostraTempo = new JTextField(); 
+	
+	private JLabel descricaoHora2 = new JLabel("Time da thread2");
+	private JTextField mostraTempo2 = new JTextField();
+	
 	public TelaTimeThread() {
-//		Configurações iniciais
+//		Configuracoes iniciais
 		setTitle("Tela de Time com Thread");
 		setSize(new Dimension(240, 240));
 		setLocationRelativeTo(null);
 		setResizable(false);
 		
+//		Segunda parte GribagConstraints Controlador de posicionamento de componentes
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
 		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);
 		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
 		
-//		Sempre será o último comando a ser executado.
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
+//		Sempre sera o ultimo comando a ser executado.
 		setVisible(true);
 	}
 
