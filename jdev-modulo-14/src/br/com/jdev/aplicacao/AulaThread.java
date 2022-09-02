bb0377f70abad6bc53596345bae24d3fc0f36670
Igor Santos Oliveira <iigor315@hotmail.com>
2022-09-02 14:22:41 -0300

    Thread em objeto e o Runnable

--- jdev-modulo-14/src/br/com/jdev/aplicacao/AulaThread.java
+++ jdev-modulo-14/src/br/com/jdev/aplicacao/AulaThread.java
@@ -6,59 +6,73 @@ import javax.swing.JOptionPane;
  * 
  * @author Igor Santos
  *
- *Curso jdev treinamentos, módulo de Thread e programação paralela ou concorrente
+ *         Curso jdev treinamentos, módulo de Thread e programação paralela ou
+ *         concorrente
  */
 public class AulaThread {
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-//		Thread processando em paralelo do envio do e-mail
-		new Thread() {
-			
-//			run executa o que nos queremos
-			public void run() {
-				
-//		Código da rotina que eu quero executar em paralelo
-				for(int pos = 0; pos <= 10; pos++) {
-					System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
 
-					try {
-						Thread.sleep(10000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-		
-//		start liga a thread que fica processando paralelamente
-		}.start();
-		
+	public static void main(String[] args) throws InterruptedException {
+
+//		Thread processando em paralelo do envio do e-mail
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
+
 		Thread.sleep(10000);
-		
+
 //		Segunda thread de processamento em paralelo
-		new Thread() {
-			
-			public void run() {
-				for(int pos = 0; pos < 10; pos ++) {
-					System.out.println("Gerando relátórios");
-					
-					try {
-						Thread.sleep(5000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				}
-			}
-			
-		}.start();
-		
+		Thread threadRelatorio = new Thread(thread2);
+		threadRelatorio.start();
 		
 //		Código do sistema do usuário continua o fluxo de trabalho
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
-		
+
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static Runnable thread1 = new Runnable() {
+		@Override
+		public void run() {
+//			Código da rotina que eu quero executar em paralelo
+			for (int pos = 0; pos <= 10; pos++) {
+				System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
+
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+				System.out.println("Gerando relátórios");
+
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+	};
+	
 }
